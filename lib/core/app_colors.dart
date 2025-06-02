52100c0040fca617f3079c389086ac09d4367970
menna-adel10 <85319557+menna-adel10@users.noreply.github.com>
2025-06-02 14:48:26 +0300

    create login view and custom text field

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -1,7 +1,12 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-class AppColors{
+import 'package:flutter/material.dart';
 
-  //static const Color kPrimaryColor = color(0xFF4C68FF);
-
+class AppColors {
+  static const Color kPrimaryColor = Color(0xFF4C68FF);
+  static const Color kScaffoldColor = Color(0xffF7F7F7);
+  static const Color kBordersideColor = Color(0xffE1E1E1);
+  static const Color kWhiteColor = Color(0xFFffffff);
+  static const Color kBlackColor = Color(0xFF17162B);
+  static const Color kGreyColor = Colors.grey;
 }
---
+++ lib/views/login_view.dart
@@ -0,0 +1,97 @@
+
+import 'package:flutter/material.dart';
+
+import '../core/app_colors.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child:
+      Center(
+        child: Column(
+          children: [
+            Text("Welcome To Our Market",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),),
+            SizedBox(
+              height: 24,
+            ),
+            Card(
+              margin: EdgeInsets.all(24),
+              color: AppColors.kWhiteColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(16),),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    CustomTextField(labelText: 'Email',),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    CustomTextField(labelText: 'password',
+                      isSecured: true,
+                      suffixIcon: IconButton(onPressed: (){}, icon: Icon(Icons.visibility_off)),
+                    ),
+
+
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      ),
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+    super.key, required this.labelText, this.suffixIcon, this.isSecured = false,
+  });
+
+  final String labelText;
+  final Widget? suffixIcon;
+  final bool isSecured;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscureText: isSecured,
+      obscuringCharacter: "*",
+      validator: (value){
+        if(value == null || value.isEmpty){
+          return "This Field is Required ";
+        }
+        return null;
+      },
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: "Email",
+        border: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: AppColors.kBordersideColor,
+            width: 2,
+          ),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: AppColors.kBordersideColor,
+            width: 2,
+          ),
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+
+    );
+  }
+}
