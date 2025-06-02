397ab4f88ab1c336133ef59dbd86c1d9ac7522b8
menna-adel10 <85319557+menna-adel10@users.noreply.github.com>
2025-06-02 15:09:07 +0300

    create custom text button

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 import '../core/app_colors.dart';
@@ -9,45 +8,84 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child:
-      Center(
-        child: Column(
-          children: [
-            Text("Welcome To Our Market",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),),
-            SizedBox(
-              height: 24,
-            ),
-            Card(
-              margin: EdgeInsets.all(24),
-              color: AppColors.kWhiteColor,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(16),),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    CustomTextField(labelText: 'Email',),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    CustomTextField(labelText: 'password',
-                      isSecured: true,
-                      suffixIcon: IconButton(onPressed: (){}, icon: Icon(Icons.visibility_off)),
-                    ),
-
-
-                  ],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Text(
+                "Welcome To Our Market",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-          ],
+              SizedBox(
+                height: 24,
+              ),
+              Card(
+                margin: EdgeInsets.all(24),
+                color: AppColors.kWhiteColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      CustomTextField(
+                        labelText: 'Email',
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      CustomTextField(
+                        labelText: 'password',
+                        isSecured: true,
+                        suffixIcon: IconButton(
+                            onPressed: () {}, icon: Icon(Icons.visibility_off)),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          CustomTextButton(text: 'Forgot Password?',
+                          onTap: (){},),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class CustomTextButton extends StatelessWidget {
+  const CustomTextButton({
+    super.key, this.onTap, required this.text,
+  });
+
+  final void Function()? onTap;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Text(
+        'text',
+        style: TextStyle(
+          color: AppColors.kPrimaryColor,
+          fontWeight: FontWeight.bold,
+        ),
       ),
     );
   }
@@ -55,7 +93,10 @@ class LoginView extends StatelessWidget {
 
 class CustomTextField extends StatelessWidget {
   const CustomTextField({
-    super.key, required this.labelText, this.suffixIcon, this.isSecured = false,
+    super.key,
+    required this.labelText,
+    this.suffixIcon,
+    this.isSecured = false,
   });
 
   final String labelText;
@@ -67,8 +108,8 @@ class CustomTextField extends StatelessWidget {
     return TextFormField(
       obscureText: isSecured,
       obscuringCharacter: "*",
-      validator: (value){
-        if(value == null || value.isEmpty){
+      validator: (value) {
+        if (value == null || value.isEmpty) {
           return "This Field is Required ";
         }
         return null;
@@ -91,7 +132,6 @@ class CustomTextField extends StatelessWidget {
           borderRadius: BorderRadius.circular(8),
         ),
       ),
-
     );
   }
 }
